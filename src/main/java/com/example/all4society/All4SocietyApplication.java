2a31e377a46cc6852c8056f9c65c74506ac7d027
주은오 <jueun-o@jueun-oui-MacBookPro.local>
2023-02-17 16:16:08 +0900

    first commit

---
+++ src/main/java/com/example/all4society/All4SocietyApplication.java
@@ -0,0 +1,13 @@
+package com.example.all4society;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class All4SocietyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(All4SocietyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/all4society/All4SocietyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.all4society;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class All4SocietyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
