0b4ad1a3544e4585d330dc90cc39d0a56b43693b
yuk <wjdrms5815@gmail.com>
2023-02-17 17:50:59 +0900

    백엔드 시작

--- src/main/java/com/example/all4society/Member.java
+++ src/main/java/com/example/all4society/Member.java
@@ -1,10 +1,15 @@
 package com.example.all4society;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
