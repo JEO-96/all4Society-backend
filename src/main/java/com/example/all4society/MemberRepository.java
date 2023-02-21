31621033d523f0e43ea32a862e07d7265af3446f
JEO-96 <37136992+JEO-96@users.noreply.github.com>
2023-02-21 11:27:47 +0900

    Update MemberRepository.java

--- src/main/java/com/example/all4society/MemberRepository.java
+++ src/main/java/com/example/all4society/MemberRepository.java
@@ -12,15 +12,15 @@ public interface MemberRepository extends JpaRepository<Member, String>{
 	@Query(value = "SELECT member_id FROM member WHERE member_id = :id and member_answer = :answer", nativeQuery = true)
 	Member findByMemberId_Answer(String id, String answer);
 
-<<<<<<< HEAD
+
 	@Query(value = "SELECT * FROM member WHERE member_name = :name AND member_phone = :phone", nativeQuery = true)
 	Member findByMemberNameAndMemberPhone(String name, String phone);
 	
 	List<Member> findAllByMemberId(String userId);
-=======
+
 	@Query(value = "SELECT member_id FROM member WHERE member_name = :memberName AND member_phone = :memberPhone", nativeQuery = true)
 	Member findByMemberNameAndMemberPhone(String memberName, String memberPhone);
 
 	Member findByMemberPhone(String memberPhone);
->>>>>>> 88deb40 (아이디 찾기 됨)
+
 }
